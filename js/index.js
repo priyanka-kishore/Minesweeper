87087ccc4a0b7961a7873675fcf5aa761140ac3a
Priyanka Kishore <prxyankakxshore@Priyankas-MBP-4.fios-router.home>
2020-06-20 13:29:34 -0400

    bomb coordinate generator

--- js/index.js
+++ js/index.js
@@ -1,21 +1,53 @@
+import * as util from './util.js'
+
 const grid = document.getElementById('grid');
 let rows = document.getElementsByClassName('gridRow');
-let cell = document.getElementsByClassName('cell');
+let cells = document.getElementsByClassName('cell');
+
+
+// beginner:       10 mines, 8x8, 9x9, 10x10
+// intermediate:   40 mines, 13x15 to 16x16
+// expert:         99 mines, 16x30
+
+let gridSize = 0;
 
 function createGrid(size) {
+    gridSize = size;
+
     console.log("calling defaultGrid");
-    for (i = 0; i < size; i++) {
+    for (var i = 0; i < gridSize; i++) {
         // create row
         let row = document.createElement('div');
         grid.appendChild(row).className = 'gridRow';
 
         // create cells in current row
-        for (j = 0; j < size; j++) {
+        for (var j = 0; j < gridSize; j++) {
             let newCell = document.createElement('div');
             newCell.innerHTML = '0';
-            row.appendChild(newCell).className = 'cell';
+            newCell.className = 'cell no-bomb no-reveal'
+
+            row.appendChild(newCell);
         }
     }
 }
 
-createGrid(15);
+createGrid(8);
+
+function placeBombs(numBombs) {
+   console.log(gridSize); 
+
+    // randomly place bombs
+    for (var i = 0; i < numBombs; i++) {
+        let x = getRandomNumber();
+        let y = getRandomNumber();
+        console.log('placing: ' + '(' + x + ', ' + y + ')');
+        
+    }
+
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * gridSize) + 1;
+}
+
+placeBombs(4);
---
+++ js/util.js
@@ -0,0 +1,9 @@
+function hi() {
+    console.log('hi from functions.js')
+}
+
+function bye() {
+    console.log('bye form functions.js')
+}
+
+export {hi, bye}
